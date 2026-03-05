2443f2e7256fd24d89a4d3215e4aec8ceabc6e54
SMILING <ahadshapklm@gmail.com>
2026-03-05 22:12:20 +0530

    feat: add L2 regularization to MLP regressor

--- nural_networks/mlp_regressor.py
+++ nural_networks/mlp_regressor.py
@@ -57,8 +57,13 @@ class MLPRegressor:
         self.B1 = None
         self.B2 = None
         self.B3 = None
+        self._Lambda =None
     def _MSE(self,y_real,y_pred):
-        return np.mean(np.square(y_pred-y_real))
+        return np.mean(np.square(y_pred-y_real))+self._Lambda*(
+            np.sum(np.square(self.W1))+
+            np.sum(np.square(self.W2))+
+            np.sum(np.square(self.W3))
+        )
 
     def _RelU(self,Z):
         return np.maximum(0,Z)
@@ -75,12 +80,12 @@ class MLPRegressor:
         dZ3 = error
         dZ2 = dZ3@W3.T*self._deriv_RelU(Z2)
         dZ1 = dZ2@W2.T*self._deriv_RelU(Z1)
-        dW3 =((-2/n)*(dZ3).T@A2).T
-        dW2 =((-2/n)*(dZ2).T@A1).T
-        dW1 =((-2/n)*(dZ1).T@x).T
-        dB3 = (-2/n) * np.sum(dZ3, axis=0, keepdims=True)
-        dB2 = (-2/n) * np.sum(dZ2, axis=0, keepdims=True)
-        dB1 = (-2/n) * np.sum(dZ1, axis=0, keepdims=True)
+        dW3 =((-2/n)*(dZ3).T@A2).T + 2*self._Lambda*W3
+        dW2 =((-2/n)*(dZ2).T@A1).T + 2*self._Lambda*W2
+        dW1 =((-2/n)*(dZ1).T@x).T  + 2*self._Lambda*W1
+        dB3 = (-2/n) * np.sum(dZ3)
+        dB2 = (-2/n) * np.sum(dZ2)
+        dB1 = (-2/n) * np.sum(dZ1)
         W1 -= dW1*lr
         W2 -= dW2*lr
         W3 -= dW3*lr
@@ -88,14 +93,14 @@ class MLPRegressor:
         B2 -= dB2*lr
         B3 -= dB3*lr
         return W1,W2,W3,B1,B2,B3
-    def fit(self,x ,y,lr=0.0001,epochs=100)->None:
+    def fit(self,x ,y,lr=0.0001,epochs=100,_Lambda = 0.001)->None:
         self.W1 = np.random.randn(x.shape[1],64) *np.sqrt(2/x.shape[1])
         self.W2 = np.random.randn(64,32) * np.sqrt(2/64)
         self.W3 = np.random.randn(32,1)  * np.sqrt(2/32)
         self.B1 = np.zeros((1, 64))
         self.B2 = np.zeros((1, 32))
         self.B3 = np.zeros((1, 1))
-        
+        self._Lambda = _Lambda
         for epoch in range(epochs):
             Z1 = self._forward(x,self.W1,self.B1)
             A1 = self._RelU(Z1)
@@ -117,18 +122,18 @@ class MLPRegressor:
         A3 = Z3
         return A3
 
-data = pd.read_csv("data/AQI.csv")
-data = data.replace([np.inf, -np.inf], np.nan) 
-data = data.dropna()  
-X = data.drop(columns=['AQI']).values
-Y = data['AQI'].values.reshape(-1,1)
+# data = pd.read_csv("data/AQI.csv")
+# data = data.replace([np.inf, -np.inf], np.nan) 
+# data = data.dropna()  
+# X = data.drop(columns=['AQI']).values
+# Y = data['AQI'].values.reshape(-1,1)
 
 #had Accuracy of R2_Score:  0.9987475375246964
 
-# data = fetch_california_housing()
-# X = data.data
-# Y = data.target
-# Y = Y.reshape(Y.shape[0],1)
+data = fetch_california_housing()
+X = data.data
+Y = data.target
+Y = Y.reshape(Y.shape[0],1)
 
 # had Accuracy score R2_Score:  0.7519517360388108
 
@@ -147,8 +152,8 @@ y_test = y_scaler.transform(y_test)
 
 np.random.seed(20)
 model = MLPRegressor()
-lr=0.01
-model.fit(x_train,y_train,lr=lr,epochs=500)
+lr=0.1
+model.fit(x_train,y_train,lr=lr,epochs=2000,_Lambda=0.0001)
 y_pred = model.predict(x_test)
 y_pred = y_scaler.inverse_transform(y_pred)
 y_test = y_scaler.inverse_transform(y_test)
